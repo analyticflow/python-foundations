56d1b2d5515a5ae44cc8bd718cc68c2b0a4e3971
analyticflow <mahekyadav30@gmail.com>
2026-06-12 19:00:36 +0530

    Added package import practice examples

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# from mypackage.calculator import add
+# from mypackage.greetings import hello
+
+# print(add(5,6))
+# hello()
+
+
+# import mypackage.calculator
+# import mypackage.greetings
+
+# print(mypackage.calculator.add(8,9))
+# mypackage.greetings.hello()
+
+
+# from mypackage.calculator import add, subtract
+
+# print(add(5,9))
+# print(subtract(99,53))
+
+# import mypackage.calculator as calc
+
+# print(calc.divide(86520,525))
+# print(calc.multiplication(852,569))
+
+
+from mypackage import add, hello
+
+print(add(5, 5))
+hello()
